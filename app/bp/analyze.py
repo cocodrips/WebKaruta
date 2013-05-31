2f81b789b42635e7b991b8e6042d9973f8a665c2
gin0606 <k.kennichi.knlnshnm@gmail.com>
2013-05-31 14:40:24 +0900

    いろいろ

--- app/bp/analyze.py
+++ app/bp/analyze.py
@@ -17,15 +17,17 @@ class Analyze():
     matching_array = []
     data = []
 
-    def __init__(self, keyword, query_num):
-        self.query_num = query_num
-        self.access_api(keyword)
-        self.compare_urls()
-
+    def __init__(self):
+        pass
 
     @property
     def summary_hash_top(self):
         return self.count_keyword(self.summaries)
+
+    def analyze(self, keyword, query_num):
+        self.query_num = query_num
+        self.access_api(keyword)
+        self.compare_urls()
 
     def access_api(self, keyword):
         appId = "83dpVeexg66u4NJrtfaO.vwNqPZTZw266pNUlLoi76oId_2S2ob.k7ygCoT0YJRdwg--"
@@ -38,45 +40,24 @@ class Analyze():
              'language':"en",
             })
 
-        response = urllib.urlopen(apiUrl+params).read()
+        response = urllib.urlopen(apiUrl + params).read()
         tree = ElementTree.fromstring(response)
 
         for url in tree.getiterator(yahooTag+"ClickUrl"):
-            data = {}
-            data["url"] = url.text
-#            soup = self.create_soup(str(url.text))
+            # XML 解析？
             dom = self.create_dom(str(url.text))
-
-#           url child
             self.extract_plain_text(dom)
             urls = self.extract_links_by_lxml(dom)
-#            urls = self.extract_links(soup)
-            if urls == "error":
-                data["childUrl"] = []
-            else:
-                data["childUrl"] = urls
+            
+            # データまとめる
+            data = {"url": url.text, "childUrl": urls}
             self.links.append(data)
-#
 
         for title in tree.getiterator(yahooTag+"Title"):
             self.titles.append(title.text)
+
         for summary in tree.getiterator(yahooTag+"Summary"):
             self.summaries.append(summary.text)
-
-    def create_soup(self, url):
-        logging.info("_______Analyze_______")
-        logging.error(url)
-
-        try:
-            html = urllib2.urlopen(url)
-        except urllib2.URLError, e:
-            if e.code >= 400:
-                return "error"
-            else:
-                return "error"
-
-        soup = BeautifulSoup.BeautifulSoup(html)
-        return soup
 
     def create_dom(self, url):
         try:
@@ -95,70 +76,55 @@ class Analyze():
             links= dom.xpath('//a')
             for _a in links:
                 href = _a.attrib['href']
-                if href != None:
+                if href !=
                     url = html_pattern.match(str(href.encode('utf_8')))
                     if url != None:
                         urls.append(href)
         except ValueError:
-            return "error"
-        return urls
-
-    def extract_links(self, soup):
-        urls = []
-        html_pattern = re.compile(r'(http://[A-Za-z0-9\'~+\-=_.,/%\?!;:@#\*&\(\)]+)')
-        try:
-            for _a in soup.findAll('a'):
-                href = _a.get('href')
-                if href != None:
-                    m = html_pattern.match(str(href.encode('utf_8')))
-                    if m != None:
-                        urls.append(href)
-        except ValueError:
-            return "error"
+            return []
         return urls
 
     def extract_plain_text(self, dom):
         try:
             body = dom.body
-            print body.text_content()
+            return body.text_content()
         except ValueError:
-            return "error"
+            return ""
 
     def compare_urls(self):
         self.matching_array =  [[False for j in range(self.query_num)] for i in range(self.query_num)]
+        hoge = self.query_num
         for i in range(self.query_num):
-            for j in range(self.query_num):
+            for j in range(hoge):
                 if i == j :
                     self.matching_array[i][j] = False
                 else:
                     self.matching_array[i][j] = self.matching_urls(self.links[i], self.links[j])
+            hoge -= 1
 
     def matching_urls(self, urls1, urls2):
         for src in urls1["childUrl"]:
-            for target in urls2["childUrl"]:
-                if src == target:
-                    return True
+            if src in urls2["childUrl"]:
+                return True
         return False
 
-
     def count_keyword(self, target):
-        hash = {}
+        words = {}
         for p in target:
-            p = p.replace('.', ' ')
-            p = p.replace(',', ' ')
+            p = p.replace('.', ' ').replace(',', ' ')
             keys = p.split(' ')
             for key in keys:
-                if key in hash:
-                    hash[key] = hash[key] + 1
+                if key in words:
+                    words[key] = words[key] + 1
                 else:
-                    hash[key] = 1
-        hash = sorted(hash.items(), key=lambda x:x[1], reverse=True)
-        return self.extract_subject_keyword(hash)
+                    words[key] = 1
+        words = sorted(words.items(), key=lambda x:x[1], reverse=True)
+        return self.extract_subject_keyword(words)
 
-    def extract_subject_keyword(self, hash):
+    def extract_subject_keyword(self, words):
         print "_______nltk_______"
 
-        hash_list = [h[0] for h in hash]
+        hash_list = [h[0] for h in words]
         tagged = nltk.pos_tag(hash_list)
         key_hash = []
         for t in tagged:
@@ -168,11 +134,6 @@ class Analyze():
                     break
         return key_hash
 
-
-
-
     @classmethod
     def create(cls, keyword, query_num):
-        analyze = Analyze(keyword, int(query_num))
-        return analyze
-
+        return Analyze(keyword, int(query_num))
